042866295859b6251d18d3fdc84405ced216be43
Ayarrih-code <ayarineal20@gmail.com>
2024-10-26 18:16:47 +0100

    adding all my files

---
+++ script.js
@@ -0,0 +1,6 @@
+// Placeholder for any JavaScript functionality
+// For example, this could handle the search function, form submissions, etc.
+document.querySelector('.search-button').addEventListener('click', () => {
+    const searchQuery = document.querySelector('.search-box').value;
+    alert('Searching for: ' + searchQuery);
+});
